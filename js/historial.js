b0c8e021f440c331635c01abd2952881638abb80
Andres <cronosreloaded@gmail.com>
2017-12-24 00:43:21 -0300

    iconos

---
+++ js/historial.js
@@ -0,0 +1,155 @@
+var historial = {
+    markerVehiculos : new Array(),
+    infowindow : "",
+    listado : "",
+    lista : "",
+    bounds : "",
+    lineSymbol : "" ,
+    recorrido : "",
+    map: "",
+    marcadores: "",
+    token : JSON.parse(localStorage.getItem("ubi-token")),
+    initMap : function(){        
+        center = this.token.centerMap;
+        centroMapaLat = parseFloat(center.split(',')[0]);
+        centroMapaLng = parseFloat(center.split(',')[1]);
+        
+        this.map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: centroMapaLat, lng: centroMapaLng},
+            zoom: 13,
+            mapTypeId: google.maps.MapTypeId.ROADMAP ,
+            disableDefaultUI: true,
+            zoomControl: true,
+            mapTypeControl: true
+        });  
+        this.lineSymbol = {path: google.maps.SymbolPath.FORWARD_OPEN_ARROW};
+        this.bounds = new google.maps.LatLngBounds();    
+        this.infowindow = new google.maps.InfoWindow();
+        this.recorrido = new google.maps.Polyline({
+            path: [],
+            icons: [{icon: this.lineSymbol,
+                scale: 15,
+                repeat: "100"}],
+            geodesic: true,
+            strokeColor: '#FF0000',
+            strokeOpacity: 1.0,
+            strokeWeight: 2
+        });
+        $(".btn-dropdown").click(function(){
+            var target = $(this).data("target");    
+            $("#"+target).toggle();
+        })
+        
+        $("#btnBuscar").click(function(){
+            historial.buscarHistorial();    
+        })
+        app.buscarVehiculos(this.agregarVehiculos);
+
+    },
+    buscarHistorial: function(){
+        var data = new Object();
+        data.vehiculo = $(".vehiculos:checked").val();
+        data.fd = $("#fechaDesde").val();
+        data.hd = $("#horaDesde").val();
+        data.fh = $("#fechaHasta").val();
+        data.hh = $("#horaHasta").val();    
+        $.ajax({
+                type: "POST",
+                url: "http://dev.ubisat.com.ar/api/usuario/getHistorial",
+                data: "data="+JSON.stringify(data),
+                success:function(data){
+                    $("#mensaje").html(data);
+                    historial.mostrarHistorial(data);                                
+                }
+            })
+    },
+    mostrarHistorial: function(lista){        
+        this.marcadores = new Array();
+        this.bounds = new google.maps.LatLngBounds();
+        this.recorrido.setMap(null);        
+        var puntos = new Array();
+        lista = JSON.parse(lista);
+        for (i=0; i<lista.length; i++){
+            puntos.push({lat: parseFloat(lista[i].latitude), lng: parseFloat(lista[i].longitude)});
+            var myLatLng = new google.maps.LatLng(parseFloat(lista[i].latitude), parseFloat(lista[i].longitude));	
+            var pos = new google.maps.LatLng(parseFloat(lista[i].latitude), parseFloat(lista[i].longitude));	
+            
+            this.bounds.extend(myLatLng);
+    
+            var scaledSize = new google.maps.Size(10,10);
+            var origin = new google.maps.Point(0,0);
+            var anchor = new google.maps.Point(0,0);
+    
+            var icono = {
+                url:"img/iconoposition/"+this.getColorIcon(lista[i].speed)+"-20.png",
+                scaledSize: scaledSize,			
+                origin: origin,
+                anchor: anchor
+            }
+    
+            var marker = new google.maps.Marker({
+              position: pos,
+              map: this.map,
+              icon: icono
+            });
+    
+            marker.id = i;
+              
+            google.maps.event.addListener(marker, 'click', function () {                	
+                var content = getIWContent(lista[this.id]);
+                infowindow.setContent(content);
+                infowindow.open(map, this);
+            });
+            this.marcadores.push(marker);   
+        }	
+        this.centrarMapa();
+        var rep = 100/lista.length*4;
+        this.recorrido.setOptions({icons: [{icon: lineSymbol,scale: 10,repeat: rep+"%"}]})
+        this.recorrido.setPath(puntos);
+    
+        this.recorrido.setMap(map);    
+    },
+    getColorIcon: function(velocidad){
+        if (velocidad == 0){
+            return "rojo";
+        }
+        if (velocidad > 10){
+            return "verde";
+        }else{
+            return "amarillo";
+        }    
+    },
+    toggleIconos: function(){
+        var newMap = null
+        if ($("#mostrarIconos").is(":checked")){
+            newMap = map
+        }
+        for(i=0; i< this.marcadores.length; i++){
+            this.marcadores[i].setMap(newMap);
+        }
+    
+    },
+    agregarVehiculos:function(){
+        let tmp_Vehiculo = $("#tmp_vehiculo").html();
+        for(i=0;i<app.vehiculos.length; i++){
+            let html= tmp_Vehiculo.replace('{{id}}', app.vehiculos[i].ID).replace('{{vehiculo}}',app.vehiculos[i].Nombre);
+            html = html.replace("{{class}}", "vehiculos");
+            $("#listaVehiculos").append(html);
+        }
+        
+        $(".vehiculos").unbind('click');   
+        
+    },
+    
+    centrarMapa: function(){
+        var markerBounds = new google.maps.LatLngBounds();
+        for(i=0;i<this.marcadores.length;i++){		
+            markerBounds.extend(this.marcadores[i].position);
+        }
+        map.fitBounds(markerBounds);
+        var listener = google.maps.event.addListener(map, "idle", function() { 
+            if (this.map.getZoom() > 13) map.setZoom(13); 
+              google.maps.event.removeListener(listener); 
+        });
+    }
+};
---
+++ js/ultimapos.js
@@ -0,0 +1,129 @@
+var ultimapos = {
+    markerVehiculos: new Array(),
+    token : JSON.parse(localStorage.getItem("ubi-token")),
+    initMap: function(){        
+        center = this.token.centerMap;
+        centroMapaLat = parseFloat(center.split(',')[0]);
+        centroMapaLng = parseFloat(center.split(',')[1]);
+    
+        map = new google.maps.Map(document.getElementById('map'), {
+              center: {lat: centroMapaLat, lng: centroMapaLng},
+              zoom: 13
+        });
+        $(".btn-dropdown").click(function(){
+            var target = $(this).data("target");    
+            $("#"+target).toggle();
+        })
+        app.buscarVehiculos(this.agregarVehiculos);
+        //this.buscarPosiciones();
+
+    },
+
+    buscarPosiciones: function(){
+        var lista = new Array();
+        $(".vehiculos").each(function(i,v){
+            if(i>0 && $(v).is(":checked")){			
+                lista.push($(v).val());
+            }
+        })
+        if (lista.length >0){
+            //$("#preloader").show();
+            $.ajax({
+                type: "POST",
+                url: "http://dev.ubisat.com.ar/api/usuario/getPosiciones",
+                data: "lista="+lista,
+                success:function(data){
+                    $("#mensaje").html(data);
+                    ultimapos.mostrarPos(JSON.parse(data));                
+                    //centrarMapa();
+                },
+                error: function(data){
+
+                }
+            })
+        }else{
+            this.borrarMarcadores();
+        }   
+    },
+    mostrarPos: function(data){
+        this.borrarMarcadores();
+        var markerBounds = new google.maps.LatLngBounds();
+        var mostrarNombre = $("#mostrarNombre").is(":checked");
+        this.markerVehiculos = new Array();
+        lista = data;
+        for(i=0;i<data.length;i++){
+            var image = {
+                url: 'img/iconovehiculos/'+this.getIcon(data[i].TipoVehiculo,data[i].speed, data[i].Icono),
+                origin: new google.maps.Point(0,0),	
+                scaledSize:  new google.maps.Size(60, 60)       	    
+            };
+            var ubicacion = { lat: parseFloat(data[i].latitude), lng: parseFloat(data[i].longitude) };	
+            
+            if (mostrarNombre){
+                var marker = new MarkerWithLabel({
+                    position: ubicacion,
+                    icon: image,
+                    map:map,
+                    labelAnchor: new google.maps.Point(40, 10),
+                    labelClass: "labels", 
+                    labelContent: data[i].name,		    
+                    labelStyle: {opacity: 0.8},
+                });
+            }else{
+                var marker = new google.maps.Marker({
+                    position: ubicacion,
+                    icon: image,
+                    map:map,			   
+                });
+            }	
+            marker.id = i;	
+            google.maps.event.addListener(marker, 'click', function () {                	
+                var content = getIWContent(lista[this.id]);
+                infowindow.setContent(content);
+                infowindow.open(map, this);
+            });
+            this.markerVehiculos.push(marker);
+            //map.setCenter(ubicacion);
+        }
+    },
+
+    agregarVehiculos: function(){
+        let tmp_Vehiculo = $("#tmp_vehiculo").html();
+        for(i=0;i<app.vehiculos.length; i++){
+            let html= tmp_Vehiculo.replace('{{id}}', app.vehiculos[i].ID).replace('{{vehiculo}}',app.vehiculos[i].Nombre);
+            html = html.replace("{{class}}", "vehiculos");
+            $("#listaVehiculos").append(html);
+        }
+        
+        $(".vehiculos").unbind('click');
+        $(".vehiculos").click(function(){        
+            if ($(this).val() == "Todos"){
+                var estado = $(this).is(":checked");
+                $(".vehiculos").each(function(i,v){
+                    $(v).prop("checked", estado);
+                })
+            }
+            ultimapos.buscarPosiciones();
+        })
+        ultimapos.buscarPosiciones();
+    },
+    getIcon: function(tipo, speed, icon){
+        var t= icon+"_";	
+        if (speed == 0){
+            t = t+"rojo.png";
+        }else if(speed > 0 && speed <=10){
+            t = t+"amarillo.png";
+        }else{
+            t = t+"verde.png";
+        }
+        return t;
+    },
+    temporizador : setInterval(this.buscarPosiciones, 15000),
+
+    borrarMarcadores: function(){
+        for(i=0;i< this.markerVehiculos.length;i++){		
+            this.markerVehiculos[i].setMap(null);
+        }
+    }
+
+}
